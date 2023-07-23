bc92052d8588c9916fa6cbc5618b222553d0121a
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-07-23 17:50:20 +0700

    Добавил альтернативное решение проверки симметричного бинарного дерева

--- src/test6.cpp
+++ src/test6.cpp
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/symmetric-tree/
+// https://leetcode.com/problems/symmetric-tree
 
 bool isSymmetric(TreeNode *left, TreeNode *right)
 {
@@ -30,13 +30,73 @@ bool isSymmetric(TreeNode *root)
     return isSymmetric(root->left, root->right);
 }
 
+bool loopIsSimmetric(TreeNode *root)
+{
+    vector<TreeNode *> nodesQueue;
+    nodesQueue.reserve(15);
+    nodesQueue.push_back(root->left);
+    nodesQueue.push_back(root->right);
+
+    while (!nodesQueue.empty())
+    {
+        auto node1 = nodesQueue.back();
+        nodesQueue.pop_back();
+        auto node2 = nodesQueue.back();
+        nodesQueue.pop_back();
+
+        if (!node1 && !node2)
+        {
+            continue;
+        }
+
+        if (!node1 || !node2)
+        {
+            return false;
+        }
+
+        if (node1->val != node2->val)
+        {
+            return false;
+        }
+
+        if (node1->left || node2->right)
+        {
+            nodesQueue.push_back(node1->left);
+            nodesQueue.push_back(node2->right);
+        }
+
+        if (node2->left || node1->right)
+        {
+            nodesQueue.push_back(node2->left);
+            nodesQueue.push_back(node1->right);
+        }
+    }
+
+    return true;
+}
+
 int main(int argc, char const *argv[])
 {
-    bool res0 = false;
+    bool res0 = false; // true
+    bool res1 = false; // true
+    bool res2 = false; // false
+    bool res3 = false; // false
 
     {
         TreeNode node{1, new TreeNode{2, new TreeNode{3}, new TreeNode{4}}, new TreeNode{2, new TreeNode{4}, new TreeNode{3}}};
         res0 = isSymmetric(&node);
+    }
+    {
+        TreeNode node{1, new TreeNode{2, new TreeNode{3}, new TreeNode{4}}, new TreeNode{2, new TreeNode{4}, new TreeNode{3}}};
+        res1 = loopIsSimmetric(&node);
+    }
+    {
+        TreeNode node{1, new TreeNode{2, new TreeNode{3}, new TreeNode{4}}, new TreeNode{2, new TreeNode{5}, new TreeNode{3}}};
+        res2 = loopIsSimmetric(&node);
+    }
+    {
+        TreeNode node{1, new TreeNode{2, new TreeNode{3}, new TreeNode{4}}, new TreeNode{2, nullptr, new TreeNode{3}}};
+        res3 = loopIsSimmetric(&node);
     }
 
     return 0;
