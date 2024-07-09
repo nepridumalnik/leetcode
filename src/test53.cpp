c7c5094f208ef4ee96851b28e7eb3284202153f4
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2024-07-09 19:52:00 +0700

    Добавлено решение

--- src/test53.cpp
+++ src/test53.cpp
@@ -2,7 +2,44 @@
 
 ListNode *mergeKLists(vector<ListNode *> &lists)
 {
-	return nullptr;
+	struct CompareNodes
+	{
+		bool operator()(const ListNode *a, const ListNode *b) const
+		{
+			return a->val < b->val;
+		}
+	};
+
+	multiset<ListNode *, CompareNodes> nodeSet;
+
+	for (ListNode *e : lists)
+	{
+		for (ListNode *node = e; node; node = node->next)
+		{
+			nodeSet.insert(node);
+		}
+	}
+
+	if (nodeSet.empty())
+	{
+		return nullptr;
+	}
+
+	std::multiset<ListNode *, CompareNodes>::iterator it = nodeSet.begin();
+	ListNode *prev = *it;
+
+	++it;
+
+	for (; it != nodeSet.end(); ++it)
+	{
+		prev->next = *it;
+		prev = prev->next;
+	}
+
+	prev->next = nullptr;
+	ListNode *node = *nodeSet.begin();
+
+	return node;
 }
 
 vector<ListNode *> makeVector(const vector<vector<int>> &v)
@@ -27,7 +64,12 @@ vector<ListNode *> makeVector(const vector<vector<int>> &v)
 
 int main(int argc, char const *argv[])
 {
+	std::vector<ListNode *> v0;
+	mergeKLists(v0);
+
 	std::vector<ListNode *> v1 = makeVector({{1, 4, 5}, {1, 3, 4}, {2, 6}});
+	ListNode *node = mergeKLists(v1);
+	delete node;
 
 	return 0;
 }
