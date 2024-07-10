385c7d98866a7cd485310f1d8d110c4f6374905d
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2024-07-10 21:27:26 +0700

    Добавлено решение

--- src/test55.cpp
+++ src/test55.cpp
@@ -2,67 +2,96 @@
 
 ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
 {
-    auto listToNum = [](const ListNode *head) -> long long
-    {
-        size_t multiplier = 1;
-        long long num = 0;
+	ListNode *result = new ListNode;
+	ListNode *ptr = result;
+	ListNode *current1 = l1;
+	ListNode *current2 = l2;
 
-        for (const ListNode *current = head; current; current = current->next)
-        {
-            num += current->val * multiplier;
-            multiplier *= 10;
-        }
+	int remain = 0;
 
-        return num;
-    };
+	while (current1 || current2)
+	{
+		int num = remain;
+		if (current1)
+		{
+			num += current1->val;
+		}
+		if (current2)
+		{
+			num += current2->val;
+		}
 
-    long long a = listToNum(l1);
-    long long b = listToNum(l2);
-    long long result = a + b;
+		if (num >= 10)
+		{
+			num -= 10;
+			remain = 1;
+		}
+		else
+		{
+			remain = 0;
+		}
 
-    ListNode *node = new ListNode;
+		ptr->val = num;
+		if (current1)
+		{
+			current1 = current1->next;
+		}
+		if (current2)
+		{
+			current2 = current2->next;
+		}
 
-    if (result)
-    {
-        ListNode *current = node;
+		if (current1 || current2)
+		{
+			ptr->next = new ListNode;
+			ptr = ptr->next;
+		}
+	}
 
-        while (result)
-        {
-            current->val = result % 10;
-            result /= 10;
+	if (remain)
+	{
+		ptr->next = new ListNode{remain};
+	}
 
-            if (result)
-            {
-                current->next = new ListNode;
-                current = current->next;
-            }
-        }
-    }
-    else
-    {
-        node->val = result;
-    }
+	return result;
+}
 
-    return node;
+unique_ptr<ListNode> addNumbers(ListNode *l1, ListNode *l2)
+{
+	return unique_ptr<ListNode>{addTwoNumbers(l1, l2)};
 }
 
 // TODO: Добавить сложение списков напрямую, а не при помощи чисел
 int main(int argc, char const *argv[])
 {
-    {
-        ListNode a{9};
-        ListNode b{1, 9, 9, 9, 9, 9, 9, 9, 9, 9};
-        unique_ptr<ListNode> result;
+	{
+		ListNode a{9};
+		ListNode b{1, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+		unique_ptr<ListNode> result;
 
-        result = unique_ptr<ListNode>(addTwoNumbers(&a, &b));
-    }
-    {
-        ListNode a{2, 4, 3};
-        ListNode b{5, 6, 4};
-        unique_ptr<ListNode> result;
+		result = addNumbers(&a, &b);
+	}
+	{
+		ListNode a{2, 4, 3};
+		ListNode b{5, 6, 4};
+		unique_ptr<ListNode> result; // 7, 0, 8
 
-        result = unique_ptr<ListNode>(addTwoNumbers(&a, &b));
-    }
+		result = addNumbers(&a, &b);
+	}
+	{
+		ListNode a{0};
+		ListNode b{0};
+		unique_ptr<ListNode> result; // 0
 
-    return 0;
+		result = addNumbers(&a, &b);
+	}
+	{
+		ListNode a{9, 9, 9, 9, 9, 9, 9};
+		ListNode b{9, 9, 9, 9};
+		unique_ptr<ListNode> result; // 8, 9, 9, 9, 0, 0, 0, 1
+
+		result = addNumbers(&a, &b);
+	}
+
+	return 0;
 }
