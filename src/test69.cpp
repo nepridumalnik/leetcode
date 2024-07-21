e8e3f4b5df97b7b895d22c125dede58d0ab0f254
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2024-07-21 18:08:50 +0700

    Добавлена задача

---
+++ src/test69.cpp
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/zigzag-conversion/description/?envType=study-plan-v2&envId=top-interview-150
+
+// TODO: решить задание
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
